2882986b95bad4c2a93886a7991c8e2e4b53d9a1
Ignacio Ricart <imr@powerstow.com>
2022-09-01 21:35:31 +0200

    add solutions until quiz2

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,19 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
+    let vec0: Vec<i32> = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    // Solution 1
+    //let vec_tmp = vec0.clone();
+    //let mut vec1 = fill_vec(vec_tmp);
+
+    // Solution 2
+    //let mut vec1 = fill_vec(vec0.clone());
+
+    // Solution 3
+    let mut vec1: Vec<i32> = Vec::new();
+    fill_vec_ref(&mut vec1);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -25,4 +32,10 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     vec.push(66);
 
     vec
+}
+
+fn fill_vec_ref(vec: &mut Vec<i32>) {
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
 }
--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,11 +1,9 @@
 // variables2.rs
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
-    if x == 10 {
+    let x: f32 = 10.0;
+    if x as i32 == 10 {
         println!("x is ten!");
     } else {
         println!("x is not ten!");
